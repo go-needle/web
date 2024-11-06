5b3ba3db8a6b5f944caaf5134fcfbc82bddab6b6
WwyDev <wwwwwydev@gmail.com>
2024-11-06 16:45:02 +0800

    Update trie.go

--- trie.go
+++ trie.go
@@ -2,18 +2,23 @@ package web
 
 import (
 	"fmt"
-	"log"
+	"strings"
 )
 
 type node struct {
-	pattern   string
+	handle    HandlerFunc
+	part      string
 	isUse     bool
 	children  map[string]*node
-	wildChild *node
-	isStop    bool // stop at '*'
+	wildChild *node //  check ':'
+	isStop    bool  // stop at '*'
 }
 
-// The first successfully matched node for insertion
+func newNode(part string) *node {
+	return &node{nil, part, false, map[string]*node{}, nil, false}
+}
+
+// The successfully matched node
 func (n *node) matchChild(part string) *node {
 	if _, has := n.children[part]; has {
 		return n.children[part]
@@ -24,64 +29,58 @@ func (n *node) matchChild(part string) *node {
 	return nil
 }
 
-// All successfully matched nodes are used to find
-func (n *node) matchChildren(part string) []*node {
-	nodes := make([]*node, 0)
-	if _, has := n.children[part]; has {
-		nodes = append(nodes, n.children[part])
-	}
-	if n.wildChild != nil {
-		nodes = append(nodes, n.wildChild)
-	}
-	return nodes
+type trieTree struct {
+	root *node
 }
 
-func (n *node) insert(pattern string, parts []string, height int) {
-	if len(parts) == height || n.isStop {
-		isSame := n.pattern == pattern
-		if n.isUse && !isSame {
-			panic(fmt.Errorf("Find the route \"%s\" is in conflict with \"%s\". It may not be safe. ", n.pattern, pattern))
+func newTrieTree() *trieTree {
+	return &trieTree{newNode("")}
+}
+
+func (t *trieTree) insert(parts []string, handlerFunc HandlerFunc) {
+	cur := t.root
+	var nodePath []string
+	for _, part := range parts {
+		next := cur.matchChild(part)
+		if next == nil {
+			next = newNode(part)
 		}
-		if n.isUse {
-			log.Printf("[Warning] the route \"%s\" is covered", n.pattern)
-		}
-		n.pattern = pattern
-		n.isUse = true
-		return
-	}
-	part := parts[height]
-	child := n.matchChild(part)
-	if child == nil {
-		child = &node{}
 		if part[0] == ':' {
-			n.wildChild = child
+			cur.wildChild = next
 		} else if part[0] == '*' {
-			child.isStop = true
-			n.wildChild = child
+			next.isStop = true
+			cur.wildChild = next
+			break
 		} else {
-			if n.children == nil {
-				n.children = make(map[string]*node)
-			}
-			n.children[part] = child
+			cur.children[part] = next
 		}
+		nodePath = append(nodePath, next.part)
+		cur = next
 	}
-	child.insert(pattern, parts, height+1)
+	if cur.isUse {
+		pattern, oldPattern := strings.Join(parts, "/"), strings.Join(nodePath, "/")
+		panic(fmt.Errorf("Find the route \"%s\" is in conflict with \"%s\". It may not be safe. ", oldPattern, pattern))
+	}
+	cur.handle = handlerFunc
+	cur.isUse = true
 }
 
-func (n *node) search(parts []string, height int) *node {
-	if len(parts) == height || n.isStop {
-		if !n.isUse {
-			return nil
+func (t *trieTree) search(parts []string) (*node, map[string]string) {
+	cur := t.root
+	params := make(map[string]string)
+	for i, part := range parts {
+		if cur.isStop {
+			params[cur.part[1:]] = strings.Join(parts[i-1:], "/")
+			break
 		}
-		return n
-	}
-	part := parts[height]
-	children := n.matchChildren(part)
-	for _, child := range children {
-		result := child.search(parts, height+1)
-		if result != nil {
-			return result
+		next := cur.matchChild(part)
+		if next == nil {
+			return nil, nil
 		}
+		if len(next.part) > 0 && next.part[0] == ':' {
+			params[cur.part[1:]] = part
+		}
+		cur = next
 	}
-	return nil
+	return cur, params
 }
